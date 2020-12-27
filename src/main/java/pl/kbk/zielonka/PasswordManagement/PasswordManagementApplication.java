b4a0a9d4a5d8574bddca69bfc55d8a7cd5398801
MrCosik <56444584+MrCosik@users.noreply.github.com>
2020-12-27 12:27:01 +0100

    Initial commit

---
+++ src/main/java/pl/kbk/zielonka/PasswordManagement/PasswordManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.kbk.zielonka.PasswordManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kbk/zielonka/PasswordManagement/PasswordManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kbk.zielonka.PasswordManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
